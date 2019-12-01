0c8af76c317d0c7dde7bf897d55bff8590a54266
mantismb <matt.d.baldwin@gmail.com>
2019-11-30 18:10:51 -0800

    Add single product details

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.get('/api/products', async (req,res) => {
 
     const [ results ] = await db.query(`
-    SELECT p.id AS id, caption, cost, p.name, i.pid AS tnId, altText, file, type 
+    SELECT p.pid AS id, caption, cost, p.name, i.pid AS tnId, altText, file, type 
     FROM products AS p 
     JOIN images AS i 
     ON i.productId=p.id
@@ -31,6 +31,52 @@ app.get('/api/products', async (req,res) => {
     });
 
     res.send({ products });
+});
+
+app.get('/api/products/:product_id', async (req, res) => {
+    
+    const { product_id } = req.params;
+
+    //Query database to get all the data you need
+    //Then, format the data as needed
+    //Then, send data to client
+
+    const [ results ] = await db.query(`
+    SELECT p.pid AS id, caption, cost, p.name, i.pid AS imId, altText, file, type, description 
+    FROM products AS p 
+    JOIN images AS i 
+    ON i.productId=p.id
+    WHERE p.pid=?
+    `, [product_id]);
+
+    let product = {};
+    let images = {};
+
+    results.forEach(result => {
+        const { imId, altText, file, type, description, ...p} = result;
+
+        let keyName = 'image';
+
+        if(type === 'thumbnail') {
+            keyName= type;
+        }
+
+        product = p;
+
+        images[keyName] = {
+            id: imId,
+            altText: altText,
+            file: file,
+            type: type,
+            url: buildUrl(req, type, file),
+        }
+        
+    });
+
+    res.send({
+        ...product,
+        ...images
+    });
 });
 
 app.listen(PORT, () => {
