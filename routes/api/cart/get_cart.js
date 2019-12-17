34851eab8510c2ae7ee4f65c4df1f1f59d09cdf0
mantismb <matt.d.baldwin@gmail.com>
2019-12-17 15:56:04 -0800

    updated get-cart

---
+++ routes/api/cart/get_cart.js
@@ -0,0 +1,24 @@
+module.exports = async (req, res, next) => {
+    try {
+        const { cart } = req;
+
+        let output = {
+            cartId: null,
+            message: "No active cart"
+        }
+
+        if(cart) {
+            output.cartId = cart.id;
+            output.message = 'You have a cart!';
+
+            // Get all cart items from db
+        }
+
+        res.send(output);
+
+    } catch(error) {
+
+        next(error);
+        
+    }
+};
